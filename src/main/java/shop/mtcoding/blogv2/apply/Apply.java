40033e55dfda074efad07215e2b8c98e3e834c9e
bbarkse93 <bbarkse93@gmail.com>
2023-08-31 15:26:55 +0900

    엔티티 생성

---
+++ src/main/java/shop/mtcoding/blogv2/apply/Apply.java
@@ -0,0 +1,41 @@
+package shop.mtcoding.blogv2.apply;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import shop.mtcoding.blogv2.notice.Notice;
+import shop.mtcoding.blogv2.resume.Resume;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "apply_tb")
+@Entity
+public class Apply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    private Resume resume;
+
+    @ManyToOne
+    private Notice notice;
+
+    @Builder
+    public Apply(Integer id, Resume resume, Notice notice) {
+        this.id = id;
+        this.resume = resume;
+        this.notice = notice;
+    }
+
+}
