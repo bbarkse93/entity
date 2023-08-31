91ad31fa3ea529cfadf0f30b9f4f2596b3f8e87f
bbarkse93 <bbarkse93@gmail.com>
2023-08-31 21:06:30 +0900

    초기세팅 완료

--- src/main/java/shop/mtcoding/blogv2/notice/NoticeController.java
+++ src/main/java/shop/mtcoding/blogv2/notice/NoticeController.java
@@ -1,8 +1,13 @@
 package shop.mtcoding.blogv2.notice;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class NoticeController {
 
+    @GetMapping("hello")
+    public String joinForm() {
+        return "notice/noticeWrite";
+    }
 }
